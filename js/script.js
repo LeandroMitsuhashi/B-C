b654a459492ceda84abdb76c669068baf2925fee
LeandroMitsuhashi <113534905+LeandroMitsuhashi@users.noreply.github.com>
2022-11-17 22:08:13 -0300

    trabalho pg area de trabalho
    modificação nas paginas pedidos e recebimento

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //alert('Olá!') colocar o alerta apenas na primeira vez do acesso com o login
 function recSalvo() {
     //alert('Lançamento Salvo')
-    var dara = new Date();
+    var data = new Date();
 
     var forma = document.getElementsByName('radforma');
 
